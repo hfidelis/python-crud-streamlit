f2b404e75f421cf4a0fea9e58a361b99cb745234
Heitor Fidelis <heitorc88@gmail.com>
2022-09-20 23:46:53 -0300

    Update database.py

--- services/database.py
+++ services/database.py
@@ -1,10 +1,16 @@
 import mysql.connector
+from os import environ
+
+HOST = environ.get('HOST', 'escreva-o-host-aqui')
+PASSWD = environ.get('PASSWD', 'escreva-o-passwd-aqui')
+USER = environ.get('USER', 'escreva-o-user-aqui')
+DATABASE = environ.get('DATABASE', 'escreva-o-db-name-aqui')
 
 bdConnection = mysql.connector.connect(
-    host='localhost',
-    user='hfidelis',
-    password='streamlitcrud123',
-    database='teste',
+    host=f'{HOST}',
+    user=f'{USER}',
+    password=f'{PASSWD}',
+    database=f'{DATABASE}',
 )  # Conexão com dados do banco de dados e MySQL
 
 cursor = bdConnection.cursor()  # Cursor para executar conexão
